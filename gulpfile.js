53657a14b23d9cf2de785a2421805199c7ad12c8
Bryan Ingle <bryaningle@gmail.com>
2016-09-05 22:10:32 -0500

    Initial commit

---
+++ gulpfile.js
@@ -0,0 +1,149 @@
+var gulp = require('gulp');
+
+var babelify = require('babelify');
+var browserify = require('browserify');
+var buffer = require('vinyl-buffer');
+var bump = require('gulp-bump');
+var git = require('gulp-git');
+var gitModified = require('gulp-gitmodified');
+var glob = require('glob');
+var helpers = require('babelify-external-helpers');
+var jasmine = require('gulp-jasmine');
+var jshint = require('gulp-jshint');
+var rename = require('gulp-rename');
+var runSequence = require('run-sequence');
+var source = require('vinyl-source-stream');
+var uglify = require('gulp-uglify');
+var util = require('gulp-util');
+
+var fs = require('fs');
+
+function getVersionFromPackage() {
+	return JSON.parse(fs.readFileSync('./package.json', 'utf8')).version;
+}
+
+function getVersionForComponent() {
+	return getVersionFromPackage().split('.').slice(0, 2).join('.');
+}
+
+gulp.task('ensure-clean-working-directory', function() {
+	return gulp.src('./**/*')
+		.pipe(gitModified('M', 'A', 'D', 'R', 'C', 'U', '??'))
+		.on('data', function (file) {
+			if (file) {
+				throw new Error('Unable to proceed, your working directory is not clean.');
+			}
+		});
+});
+
+gulp.task('bump-version', function () {
+	return gulp.src([ './package.json' ])
+		.pipe(bump({ type: 'patch' }).on('error', util.log))
+		.pipe(gulp.dest('./'));
+});
+
+gulp.task('commit-changes', function () {
+	return gulp.src([ './', './dist/' ])
+		.pipe(git.add())
+		.pipe(gitModified('M', 'A'))
+		.pipe(git.commit('Release. Bump version number'));
+});
+
+gulp.task('push-changes', function (cb) {
+	git.push('origin', 'master', cb);
+});
+
+gulp.task('create-tag', function (cb) {
+	var version = getVersionFromPackage();
+
+	git.tag(version, 'Release ' + version, function (error) {
+		if (error) {
+			return cb(error);
+		}
+
+		git.push('origin', 'master', { args: '--tags' }, cb);
+	});
+});
+
+gulp.task('build-browser', function() {
+	return browserify('./index.js')
+		.transform('babelify', {presets: ['es2015']})
+		.bundle()
+		.pipe(source('jsonpack-' + getVersionForComponent() + '.js'))
+		.pipe(buffer())
+		.pipe(gulp.dest('./dist'))
+		.pipe(uglify())
+		.pipe(rename('jsonpack-' + getVersionForComponent() + '-min.js'))
+		.pipe(gulp.dest('dist/'));
+});
+
+gulp.task('build-browser-tests', function () {
+	return browserify({ entries: glob.sync('test/specs/**/*.js') })
+		.transform('babelify', {presets: ['es2015']})
+		.bundle()
+		.pipe(source('jsonpack-tests-' + getVersionForComponent() + '.js'))
+		.pipe(buffer())
+		.pipe(gulp.dest('test/dist'));
+});
+
+gulp.task('build-browser-components', [ 'build-browser' ]);
+
+gulp.task('build', [ 'build-browser-components' ]);
+
+gulp.task('execute-browser-tests', function () {
+	return gulp.src('test/dist/jsonpack-tests-' + getVersionForComponent() + '.js')
+		.pipe(jasmine());
+});
+
+gulp.task('execute-node-tests', function () {
+	return gulp.src(['index.js', 'test/specs/**/*.js'])
+		.pipe(jasmine());
+});
+
+gulp.task('execute-tests', function (callback) {
+	runSequence(
+		'build-browser-tests',
+		'execute-browser-tests',
+		'execute-node-tests',
+
+		function (error) {
+			if (error) {
+				console.log(error.message);
+			}
+
+			callback(error);
+		});
+});
+
+gulp.task('release', function (callback) {
+	runSequence(
+		'ensure-clean-working-directory',
+		'build',
+		'build-browser-tests',
+		'execute-browser-tests',
+		'execute-node-tests',
+		'bump-version',
+		'commit-changes',
+		'push-changes',
+		'create-tag',
+
+		function (error) {
+			if (error) {
+				console.log(error.message);
+			} else {
+				console.log('Release complete');
+			}
+
+			callback(error);
+		});
+});
+
+gulp.task('lint', function() {
+	return gulp.src([ './**/*.js', './test/specs/**/*.js', '!./node_modules/**', '!./dist/**', '!./test/dist/**' ])
+		.pipe(jshint({'esversion': 6}))
+		.pipe(jshint.reporter('default'));
+});
+
+gulp.task('test', [ 'execute-tests' ]);
+
+gulp.task('default', [ 'lint' ]);
---
+++ test/specs/schema/header/headerSpec.js
@@ -0,0 +1,110 @@
+var header = require('./../../../../lib/schema/header/header');
+
+describe('when generating a header byte', () => {
+	'use strict';
+
+	describe('for a value that exists', () => {
+		let index;
+		let value;
+
+		let byte;
+
+		beforeEach(() => {
+			byte = header.getByte(index = 13, value = 'a byte that is neither null or undefined');
+		});
+
+		it('the header byte should be a number', () => {
+			expect(typeof byte).toEqual('number');
+		});
+
+		it('the header byte should be one byte long', () => {
+			expect(byte >>> 8).toEqual(0);
+		});
+
+		it ('the header byte should contain the field index', () => {
+			expect(header.getIndex(byte)).toEqual(index);
+		});
+
+		it ('the header byte should indicate a value is present', () => {
+			expect(header.getValueIsPresent(byte)).toEqual(true);
+		});
+
+		it ('the header byte should not indicate a null value', () => {
+			expect(header.getValueIsNull(byte)).toEqual(false);
+		});
+
+		it ('the header byte should not indicate an undefined value', () => {
+			expect(header.getValueIsUndefined(byte)).toEqual(false);
+		});
+	});
+
+	describe('for a null value', () => {
+		let index;
+		let value;
+
+		let byte;
+
+		beforeEach(() => {
+			byte = header.getByte(index = 13, value = null);
+		});
+
+		it('the header byte should be a number', () => {
+			expect(typeof byte).toEqual('number');
+		});
+
+		it('the header byte should be one byte long', () => {
+			expect(byte >>> 8).toEqual(0);
+		});
+
+		it ('the header byte should contain the field index', () => {
+			expect(header.getIndex(byte)).toEqual(index);
+		});
+
+		it ('the header byte should indicate a value is not present', () => {
+			expect(header.getValueIsPresent(byte)).toEqual(false);
+		});
+
+		it ('the header byte should indicate a null value', () => {
+			expect(header.getValueIsNull(byte)).toEqual(true);
+		});
+
+		it ('the header byte should not indicate an undefined byte', () => {
+			expect(header.getValueIsUndefined(byte)).toEqual(false);
+		});
+	});
+
+	describe('for an undefined value', () => {
+		let index;
+		let value;
+
+		let byte;
+
+		beforeEach(() => {
+			byte = header.getByte(index = 13, value = undefined);
+		});
+
+		it('the header byte should be a number', () => {
+			expect(typeof byte).toEqual('number');
+		});
+
+		it('the header byte should be one byte long', () => {
+			expect(byte >>> 8).toEqual(0);
+		});
+
+		it ('the header byte should contain the field index', () => {
+			expect(header.getIndex(byte)).toEqual(index);
+		});
+
+		it ('the header byte should indicate a value is not present', () => {
+			expect(header.getValueIsPresent(byte)).toEqual(false);
+		});
+
+		it ('the header byte should not indicate a null value', () => {
+			expect(header.getValueIsNull(byte)).toEqual(false);
+		});
+
+		it ('the header byte should indicate an undefined byte', () => {
+			expect(header.getValueIsUndefined(byte)).toEqual(true);
+		});
+	});
+});

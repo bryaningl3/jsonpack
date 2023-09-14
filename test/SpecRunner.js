9025261f88627fa0232b03d03f4b78d128001971
Paradela0 <joseantoniovieira.p@gmail.com>
2023-09-14 23:21:27 +0100

    wergthyj

--- test/SpecRunner.js
+++ test/SpecRunner.js
@@ -1,19 +1,4 @@
-(function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
-const is = require('./../../../utilities/is');
 
-module.exports = (() => {
-  'use strict';
-
-  const PRESENT_MASK = 1 << 6;
-  const UNDEFINED_MASK = 1 << 7;
-  const FIELD_MASK = PRESENT_MASK | UNDEFINED_MASK;
-  const header = {
-    getByte(index, value) {
-      let byte;
-
-      if (is.null(value)) {
-        byte = index;
-      } else if (is.undefined(value)) {
         byte = index | UNDEFINED_MASK;
       } else {
         byte = index | PRESENT_MASK;
